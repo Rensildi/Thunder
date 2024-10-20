de3e8b170385f6fb990b525c848b7fbbf700b9a6
wumbo3000 <parkersmith555@gmail.com>
2024-10-20 19:51:57 -0400

    Changed program to uilize frames within GUI - consolidating all content to one window instead of opening/closing GUI windows during program navigation. Added a back-o-sign-in button to the sign up page, as well as an automatic return to the sign in page upon successful account creation. Additionaly, switched from MySQL to SQLite - supporting a localized database with no need for end user setup.

---
+++ Project 2/main.py
@@ -0,0 +1,30 @@
+from customtkinter import *
+from SignIn import SignIn
+from SignUp import SignUp
+
+class MainApp:
+    def __init__(self):
+        print("Initializing MainApp")  # Debugging statement
+        self.root = CTk()  # Create the main application window
+        self.root.geometry("900x600")
+        self.root.title("Thunder")
+
+        self.sign_in_screen = SignIn(self)  # Pass self
+        self.sign_up_screen = SignUp(self)  # Pass self
+
+        self.sign_in_screen.pack(fill='both', expand=True)  # Show sign in screen
+
+    def run(self):
+        self.root.mainloop()
+
+    def show_signin(self):
+        self.sign_up_screen.pack_forget()  # Hide SignUp if it's open
+        self.sign_in_screen.pack(fill='both', expand=True)  # Show SignIn
+
+    def show_signup(self):
+        self.sign_in_screen.pack_forget()  # Hide SignIn if it's open
+        self.sign_up_screen.pack(fill='both', expand=True)  # Show SignUp
+
+if __name__ == "__main__":
+    app = MainApp()  # Create an instance of MainApp
+    app.run()  # Run the application 
